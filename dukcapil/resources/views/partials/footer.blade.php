268844d2bc5f75cb5b840e705fd640cdf6dd1edc
rapaccel <rap4ccel@gmail.com>
2023-11-16 22:34:31 +0700

    hapus folder tidak terpakai

--- dukcapil/resources/views/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <div class="footer clearfix mb-0 text-muted p-4">
-      <div class="float-end">
-        <p>2023 &copy; Dukcapil Gunung Kidul</p>
-      </div>
-      
-    </div>
-  </footer>
